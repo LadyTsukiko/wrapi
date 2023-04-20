3bfaa174aff5c7170c0dfb0434e31d7bcfe90db9
Alexandra de Groof <degroof.alexandra@gmail.com>
2023-04-20 11:32:03 +0200

    more fix

--- wrapi/types_/inputs.py
+++ wrapi/types_/inputs.py
@@ -54,7 +54,9 @@ class WorkScheduleFields(str, CapitalizedExceptFirstNameEnum):
     user_ids = auto()
 
 
-class TaskOptionalFields(ShortTaskOptionalFields, CapitalizedExceptFirstNameEnum):
+class TaskOptionalFields(str, CapitalizedExceptFirstNameEnum):
+    EFFORT_ALLOCATION = auto()
+    BILLING_TYPE = auto()
     AUTHOR_IDS = auto()
     HAS_ATTACHMENTS = auto()
     ATTACHMENT_COUNT = auto()
